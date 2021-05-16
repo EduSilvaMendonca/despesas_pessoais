53a3ba62705a639ce974992f8ce96130206f972d
Eduardo Mendonca <edusilvamendonca@gmail.com>
2021-05-16 16:29:04 -0400

    Adicionando Transação parte 01

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:expenses/components/transaction_list.dart';
@@ -23,6 +25,20 @@ class _TransactionUserState extends State<TransactionUser> {
       date: DateTime.now(),
     ),
   ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
