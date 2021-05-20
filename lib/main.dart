8559ce9a86e3ee1b1b18abc296495661880f13ea
Eduardo Mendonca <edusilvamendonca@gmail.com>
2021-05-19 21:48:26 -0400

    Melhorando interface emmodo paisagem

--- lib/main.dart
+++ lib/main.dart
@@ -89,11 +89,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: Text('Despesas Pessoais'),
       actions: [
+        if (isLandscape)
+          IconButton(
+            icon: Icon(_showChart ? Icons.list : Icons.show_chart),
+            onPressed: () {
+              setState(() {
+                _showChart = !_showChart;
+              });
+            },
+          ),
         IconButton(
           icon: Icon(
             Icons.add,
@@ -113,27 +123,12 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            if (isLandscape)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Exibir Gráfico'),
-                  Switch(
-                    value: _showChart,
-                    onChanged: (value) {
-                      setState(() {
-                        _showChart = value;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            if(_showChart || !isLandscape) 
+            if (_showChart || !isLandscape)
               Container(
                 height: availableHeight * (isLandscape ? 0.7 : 0.25),
                 child: Chart(_recentTransactions),
               ),
-            if(!_showChart || !isLandscape) 
+            if (!_showChart || !isLandscape)
               Container(
                 height: availableHeight * 0.75,
                 child: TransactionList(_transactions, _removeTransaction),
